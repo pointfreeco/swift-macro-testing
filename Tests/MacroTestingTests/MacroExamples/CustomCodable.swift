35acd9468d40ae87e75991a18af6271e8124c261
stephencelis <stephencelis@users.noreply.github.com>
2023-10-06 21:05:56 +0000

    Run swift-format

--- Tests/MacroTestingTests/MacroExamples/CustomCodable.swift
+++ Tests/MacroTestingTests/MacroExamples/CustomCodable.swift
@@ -24,18 +24,22 @@ public enum CustomCodable: MemberMacro {
     let cases = memberList.compactMap({ member -> String? in
       // is a property
       guard
-        let propertyName = member.decl.as(VariableDeclSyntax.self)?.bindings.first?.pattern.as(IdentifierPatternSyntax.self)?.identifier.text
+        let propertyName = member.decl.as(VariableDeclSyntax.self)?.bindings.first?.pattern.as(
+          IdentifierPatternSyntax.self)?.identifier.text
       else {
         return nil
       }
 
       // if it has a CodableKey macro on it
-      if let customKeyMacro = member.decl.as(VariableDeclSyntax.self)?.attributes.first(where: { element in
-        element.as(AttributeSyntax.self)?.attributeName.as(IdentifierTypeSyntax.self)?.description == "CodableKey"
+      if let customKeyMacro = member.decl.as(VariableDeclSyntax.self)?.attributes.first(where: {
+        element in
+        element.as(AttributeSyntax.self)?.attributeName.as(IdentifierTypeSyntax.self)?.description
+          == "CodableKey"
       }) {
 
         // Uses the value in the Macro
-        let customKeyValue = customKeyMacro.as(AttributeSyntax.self)!.arguments!.as(LabeledExprListSyntax.self)!.first!.expression
+        let customKeyValue = customKeyMacro.as(AttributeSyntax.self)!.arguments!.as(
+          LabeledExprListSyntax.self)!.first!.expression
 
         return "case \(propertyName) = \(customKeyValue)"
       } else {
