8324875714e778bf07009174965058cfe0fe6b50
Stephen Celis <stephen@stephencelis.com>
2024-03-19 18:04:44 -0700

    wip

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-syntax", "509.0.0"..<"511.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.15.0"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", branch: "inline-snapshot-removal"),
   ],
   targets: [
     .target(
