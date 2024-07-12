9bffcf74894012736298b422b6d80dec2c12aae2
Stephen Celis <stephen@stephencelis.com>
2024-07-12 12:56:34 -0700

    Point updated packages to swiftlang organization

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-syntax", "509.0.0"..<"601.0.0-prerelease"),
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.1"),
+    .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"601.0.0-prerelease"),
   ],
   targets: [
     .target(
