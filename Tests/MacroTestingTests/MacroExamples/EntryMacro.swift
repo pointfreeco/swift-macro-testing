3a1e653435a0551fbad9611d1216c9f22640ec54
Cameron Pulsford <cpulsfor@me.com>
2024-08-28 15:47:17 -0400

    Add support for lexical context (#31)
    * Pass lexical context into the BasicMacroExpensionContext.
    
    * Add basic tests to ensure lexical context is supplied by assertMacro.

---
+++ Tests/MacroTestingTests/MacroExamples/EntryMacro.swift
@@ -0,0 +1,25 @@
+import SwiftSyntax
+import SwiftSyntaxMacros
+
+// Not complete, just enough to unit test lexical context.
+public struct EntryMacro: AccessorMacro {
+  public static func expansion(
+    of node: AttributeSyntax,
+    providingAccessorsOf declaration: some DeclSyntaxProtocol,
+    in context: some MacroExpansionContext
+  ) throws -> [AccessorDeclSyntax] {
+    let isInEnvironmentValues = context.lexicalContext.contains { lexicalContext in
+      lexicalContext.as(ExtensionDeclSyntax.self)?.extendedType.trimmedDescription == "EnvironmentValues"
+    }
+
+    guard isInEnvironmentValues else {
+      throw MacroExpansionErrorMessage("'@Entry' macro can only attach to var declarations inside extensions of EnvironmentValues")
+    }
+
+    return [
+      AccessorDeclSyntax(accessorSpecifier: .keyword(.get)) {
+        "fatalError()"
+      },
+    ]
+  }
+}
