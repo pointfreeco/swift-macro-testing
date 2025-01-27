bab9c9bac6234d1fad2b7beaccf07bd71112ed36
Johan Kool <johankool@users.noreply.github.com>
2025-01-28 00:52:49 +0100

    Compile for Android

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/johankool/swift-snapshot-testing", branch: "android-support"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.0"),
     .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"601.0.0-prerelease"),
   ],
   targets: [
